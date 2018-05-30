c324676b7bfae82507e51dc384ea6636673ba0bc
Choi Jae-Yong <jayong93@gmail.com>
2018-05-30 15:04:27 +0900

    카카오 3차 5번 해결

--- KaKao3-5.py
+++ KaKao3-5.py
@@ -5,7 +5,7 @@ class Node:
 
 class Trie:
     def __init__(self):
-        self.root = Node('')
+        self.root = Node()
 
     def insert_words(self, word):
         cur_node = self.root
@@ -16,12 +16,42 @@ class Trie:
             else:
                 break
 
-        for char in word[i:]:
-            pass
+        for j, char in enumerate(word[i:]):
+            cur_node.children[char] = Node()
+            cur_node = cur_node.children[char]
+        cur_node.value = word
 
     def count_chr_to_find_word(self, word):
-        pass
+        cur_node = self.root
+        count = 0
 
+        for char in word:
+            cur_node = cur_node.children[char]
+            count += 1
+            if self._is_unique(cur_node, word):
+                break
+
+        return count
+
+    def _is_unique(self, cur_node, word):
+        while len(cur_node.children) > 0:
+            if len(cur_node.children) > 1:
+                return False
+            if cur_node.value is not None:
+                return False
+            cur_node = list(cur_node.children.values())[0]
+
+        return True
 
 if __name__ == "__main__":
-    trie = Trie()
+    inputs = [["go", "gone", "guild"], ["abc", "def", "ghi", "jklm"], ["word", "war", "warrior", "world"]]
+
+    for inp in inputs:
+        count = 0
+        trie = Trie()
+        for word in inp:
+            trie.insert_words(word)
+        for word in inp:
+            count += trie.count_chr_to_find_word(word)
+
+        print(count)
